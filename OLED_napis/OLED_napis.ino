7939f0e7774e9f66bd261eb40587b0dbcb5bdd6f
Fajerancik <141372171+Fajerancik@users.noreply.github.com>
2023-08-04 00:19:45 +0200

    Add OLED_napis

---
+++ OLED_napis/OLED_napis.ino
@@ -0,0 +1,50 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+void setup() {
+  Serial.begin(115200);
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  delay(2000);
+  display.clearDisplay();
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 1);
+  // Display static text
+
+}
+
+void loop() {
+  display.setCursor(30,10);
+
+  display.println("Guten tag!");
+  display.display(); 
+  delay(2000);
+  display.clearDisplay();
+  
+  display.setCursor(30,20);
+  display.println("Guten abend!");
+  display.display(); 
+  delay(2000);
+  display.clearDisplay();
+
+  display.setCursor(30,30);
+  display.println("Auwidersehen!");
+  display.display(); 
+  delay(2000);
+  display.clearDisplay();
+
+
+
+}

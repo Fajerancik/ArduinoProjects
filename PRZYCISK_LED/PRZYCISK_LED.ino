a6f6e1fe187f6b8749a6c08c467b07b9a3705d26
Fajerancik <141372171+Fajerancik@users.noreply.github.com>
2024-06-08 15:59:51 +0200

    Update PRZYCISK_LED.ino

--- PRZYCISK_LED/PRZYCISK_LED.ino
+++ PRZYCISK_LED/PRZYCISK_LED.ino
@@ -1,28 +1,70 @@
 int buttonPin = 2;
-int ledPin = 13;
+int redPin = 9;
+int greenPin = 10;
+int bluePin = 11;
 
 int buttonState = 0;
 int lastButtonState = 0;
+int colorState = 0;
 
 void setup() {
-  pinMode(buttonPin, INPUT); 
-  pinMode(ledPin, OUTPUT);
-  digitalWrite(ledPin, LOW); 
+  pinMode(buttonPin, INPUT);
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+  setColor(0);
 }
 
 void loop() {
-  buttonState = digitalRead(buttonPin); 
+  buttonState = digitalRead(buttonPin);
 
- 
   if (buttonState != lastButtonState) {
     if (buttonState == HIGH) {
-      digitalWrite(ledPin, HIGH);
-    }else{
-      digitalWrite(ledPin, LOW);
+      colorState = (colorState + 1) % 7;
+      setColor(colorState);
     }
-
     delay(50);
   }
 
-  lastButtonState = buttonState; 
+  lastButtonState = buttonState;
+}
+
+void setColor(int state) {
+  switch (state) {
+    case 0:
+      analogWrite(redPin, 0);
+      analogWrite(greenPin, 0);
+      analogWrite(bluePin, 0);
+      break;
+    case 1:
+      analogWrite(redPin, 255);
+      analogWrite(greenPin, 0);
+      analogWrite(bluePin, 0);
+      break;
+    case 2:
+      analogWrite(redPin, 0);
+      analogWrite(greenPin, 255);
+      analogWrite(bluePin, 0);
+      break;
+    case 3:
+      analogWrite(redPin, 0);
+      analogWrite(greenPin, 0);
+      analogWrite(bluePin, 255);
+      break;
+    case 4:
+      analogWrite(redPin, 255);
+      analogWrite(greenPin, 255);
+      analogWrite(bluePin, 0);
+      break;
+    case 5:
+      analogWrite(redPin, 255);
+      analogWrite(greenPin, 0);
+      analogWrite(bluePin, 255);
+      break;
+    case 6:
+      analogWrite(redPin, 0);
+      analogWrite(greenPin, 255);
+      analogWrite(bluePin, 255);
+      break;
+  }
 }
